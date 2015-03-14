f65e845c63c8a3e0e911f9437753bb73f93b5a03
cnichols <mellowdious@mail.com>
2015-03-14 16:08:24 -0700

    removed mailgun unit test

--- test/unit/mailgun.js
+++ test/unit/mailgun.js
@@ -1,28 +1,28 @@
-/* jshint expr:true */
+// /* jshint expr:true */
 
-'use strict';
+// 'use strict';
 
-require('babel/register');
+// require('babel/register');
 
-var expect = require('chai').expect;
-var Lab = require('lab');
-var lab = exports.lab = Lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var beforeEach = lab.beforeEach;
-require('../../server/index');
-var mailgun = require ('../../server/models/mailgun');
+// var expect = require('chai').expect;
+// var Lab = require('lab');
+// var lab = exports.lab = Lab.script();
+// var describe = lab.describe;
+// var it = lab.it;
+// var beforeEach = lab.beforeEach;
+// require('../../server/index');
+// var mailgun = require ('../../server/models/mailgun');
 
-describe('Mailgun', function() {
-  beforeEach(function(done) {
-    done();
-  });
-  describe('send email', function() {
-    it('should send an email', function(done) {
-      mailgun.send(function(err){
-        console.log('MAILGUN ERR:', err);
-        done();
-      });
-    });
-  });  
-});
+// describe('Mailgun', function() {
+//   beforeEach(function(done) {
+//     done();
+//   });
+//   describe('send email', function() {
+//     it('should send an email', function(done) {
+//       mailgun.send(function(err){
+//         console.log('MAILGUN ERR:', err);
+//         done();
+//       });
+//     });
+//   });  
+// });
