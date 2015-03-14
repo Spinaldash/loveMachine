c6b84bf9c95ab684e7dba5fa8467459eaa444a9f
Jose Zamudio <zamudio@outlook.com>
2015-03-14 10:42:28 -0700

    Save in localstorage after registration

--- client/models/user.js
+++ client/models/user.js
@@ -3,9 +3,8 @@
 angular.module('dating-app')
   .factory('User', ['$http', function($http){
 
-    function register(user) {
-      console.log('User', user);
-      // return $http.post(`/users/${user._id}`, user);
+    function register(userId,user) {
+      return $http.post(`/users/${userId}`, user);
     }
 
     return {register:register};
--- client/views/users/register.controller.js
+++ client/views/users/register.controller.js
@@ -4,8 +4,11 @@ angular.module('dating-app')
   .controller('RegisterCtrl', ['$rootScope', '$scope', '$window', '$state', '$auth', 'User', function($rootScope, $scope, $window, $state, $auth, User){
 
     $scope.finish = function(user) {
-      User.register(user)
+      User.register($rootScope.user._id, user)
       .then(response => {
+        $window.localStorage.user = JSON.stringify(response.data.user);
+        $rootScope.user = response.data.user;
+        console.log(response.data);
         $state.go('login');
       });
     };
