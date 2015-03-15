6fd422b42c3e486529dbfe2d346c0fa398f92c13
Spinaldash <blueavenger@gmail.com>
2015-03-14 20:20:44 -0700

    LookingFor checkboxes

--- client/views/users/register.controller.js
+++ client/views/users/register.controller.js
@@ -3,6 +3,11 @@
 angular.module('dating-app')
   .controller('RegisterCtrl', ['$rootScope', '$scope', '$window', '$state', '$auth', 'User', function($rootScope, $scope, $window, $state, $auth, User){
 
+    $scope.colors ={Green: true, Orange: false};
+    $scope.newUser ={};
+    $scope.newUser.lookingFor = {Male: false, Female: false, Other: false};
+
+
     $scope.finish = function(user) {
       User.register($rootScope.user._id, user)
       .then(response => {
