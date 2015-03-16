e1c5fd53152534744a927556d988b7dfaed24999
Spinaldash <blueavenger@gmail.com>
2015-03-16 10:08:42 -0700

    form additions

--- client/views/users/register.controller.js
+++ client/views/users/register.controller.js
@@ -3,7 +3,6 @@
 angular.module('dating-app')
   .controller('RegisterCtrl', ['$rootScope', '$scope', '$window', '$state', '$auth', 'User', function($rootScope, $scope, $window, $state, $auth, User){
 
-    $scope.colors ={Green: true, Orange: false};
     $scope.newUser ={};
     $scope.newUser.lookingFor = {Male: false, Female: false, Other: false};
 
