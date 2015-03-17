8a10cdaf2fdb6939b4255662c7f4f66ab0a467ca
Jose Zamudio <zamudio@outlook.com>
2015-03-16 19:19:50 -0700

    More fixes

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -4,18 +4,17 @@ let User = require('../../models/user');
 
 module.exports = {
   handler: function(request, reply){
-    let params = {};
-    // let params = {$and: [{$or: []}]};
-    //
-    // for(var pref in request.auth.credentials.lookingFor) {
-    //   if(request.auth.credentials.lookingFor[pref]){
-    //     params.$and[0].$or.push({gender:pref});
-    //   }
-    // }
+    let params = {$and: [{$or: []}]};
 
-    // let prefQuery = {};
-    // prefQuery['lookingFor.' + request.auth.credentials.gender] = true;
-    // params.$and.push(prefQuery);
+    for(var pref in request.auth.credentials.lookingFor) {
+      if(request.auth.credentials.lookingFor[pref]){
+        params.$and[0].$or.push({gender:pref});
+      }
+    }
+
+    let prefQuery = {};
+    prefQuery['lookingFor.' + request.auth.credentials.gender] = true;
+    params.$and.push(prefQuery);
 
     User.find(params, (err, users) => {
       if(err){reply().code(400);}
