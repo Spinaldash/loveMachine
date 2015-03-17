ec72477f449f786c37ca0eb0204121c1eec8af9a
Tommy Janszen <tommy.janszen@gmail.com>
2015-03-16 22:50:41 -0700

    Revert "Update the update user form"
    This reverts commit aac4132dfa8e66438b83b3c33d94fff6925937b4.

--- client/views/general/home.controller.js
+++ client/views/general/home.controller.js
@@ -3,9 +3,6 @@
 angular.module('dating-app')
   .controller('HomeCtrl', ['$rootScope', '$scope', '$state', 'User', function($rootScope, $scope, $state, User){
 
-    $scope.filterUser = {};
-    $scope.filterUser.age = $scope.user.age
-    $scope.filterUser.location = $scope.user.location;
     $scope.newUser ={};
     $scope.newUser.lookingFor = {Male: false, Female: false, Other: false};
 
@@ -13,14 +10,9 @@ angular.module('dating-app')
       $state.go('/');
     }
 
-
     User.findAll()
     .then(response => {
       $scope.users = response.data.users;
     });
-
-    $scope.ageFilter = function(user) {
-      return user.age <= $scope.filterUser.age;
-    }
 
   }]);
