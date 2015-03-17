9941adb55e1de50074ed8169bbf44f8d37f3e008
Spinaldash <blueavenger@gmail.com>
2015-03-16 23:06:31 -0700

    history customfilter1

--- client/views/account/history.controller.js
+++ client/views/account/history.controller.js
@@ -3,10 +3,24 @@
 angular.module('dating-app')
   .controller('AccountHistoryCtrl', ['$rootScope', '$scope', '$window', '$state', 'User', function($rootScope, $scope, $window, $state, User){
 
-    console.log('RUNNING PRE PROPOSAL RESPONSE');
+    $scope.proposal1filter = function(proposal) {
+      if(proposal.sender._id === $rootScope.user._id) {
+        return false;
+      }
+      return true;
+    }
+
     User.getProposals()
     .then(response => {
-      console.log('RUNNING GETT PROPOSAL RESPONSE');
       $scope.proposals = response.data.proposals;
     });
   }]);
+
+  //
+  // $scope.item1filter = function(trade) {
+  //       if(trade.user1._id === $rootScope.userId && !trade.isCompleted) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     };
