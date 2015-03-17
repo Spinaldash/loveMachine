a5e26ccccc438726c37fda2028d6e2b701b91bf7
cnichols <mellowdious@mail.com>
2015-03-16 20:56:08 -0700

    removed console.logs

--- server/routes/items/create.js
+++ server/routes/items/create.js
@@ -4,11 +4,9 @@ var Item = require('../../models/item');
 
 module.exports = {
   handler: function(request, reply){
-    console.log('CREDENTIALS', request.auth.credentials);
     if(!request.auth.credentials.admin){
       reply().code(401);
     }else{
-      console.log('PAYLOAD', request.payload);
       var item = new Item(request.payload);
       item.save(function() {
         reply();
