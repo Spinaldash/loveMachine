dee07beb847211e8ed7f9d49250b897ad90ed39c
Spinaldash <blueavenger@gmail.com>
2015-03-17 00:01:32 -0700

    accept/deny requests online

--- client/views/account/history.controller.js
+++ client/views/account/history.controller.js
@@ -1,19 +1,34 @@
 'use strict';
 
 angular.module('dating-app')
-  .controller('AccountHistoryCtrl', ['$rootScope', '$scope', '$window', '$state', 'User', function($rootScope, $scope, $window, $state, User){
+  .controller('AccountHistoryCtrl', ['$rootScope', '$scope', '$window', '$state', 'User', 'Dates', function($rootScope, $scope, $window, $state, User, Dates){
 
     $scope.proposal1filter = function(proposal) {
       if(proposal.sender._id === $rootScope.user._id) {
         return false;
       }
       return true;
+    };
+
+    $scope.declineProposal = function(propId) {
+      Dates.decline(propId)
+      .then(()=>{
+        $state.reload();
+      })
+    }
+
+    $scope.acceptProposal = function(propId) {
+      Dates.agree(propId)
+      .then(() => {
+        $state.reload();
+      })
     }
 
     User.getProposals()
     .then(response => {
       $scope.proposals = response.data.proposals;
     });
+
   }]);
 
   //
